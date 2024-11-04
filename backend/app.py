c71bfb518c2f1291b3147a6c70f3d12c69f0b146
Aziiimm <azimrahat108@gmail.com>
2024-11-04 02:45:54 -0500

    connected to heroku

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
 from api.predict import predict_app  # Import the blueprint
+from flask import jsonify
 import os
 
 app = Flask(__name__)
@@ -8,6 +9,10 @@ app = Flask(__name__)
 CORS(app, resources={r"/api/*": {"origins": "http://localhost:5173"}})
 
 app.register_blueprint(predict_app, url_prefix='/api')  # Register the blueprint
+
+@app.route('/')
+def home():
+    return jsonify({"message": "Welcome to the ShotCaller API. Use /api/predict for predictions."})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
