962f44ed60a236c306e259d1f795f6c1d1cfa4a1
Aziiimm <azimrahat108@gmail.com>
2024-11-04 02:08:08 -0500

    setting up heroku

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from api.predict import app as predict_app
+import os
+
 app = Flask(__name__)
 
-# Include other routes or functions here if necessary
+app.register_blueprint(predict_app, url_prefix='/api')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
