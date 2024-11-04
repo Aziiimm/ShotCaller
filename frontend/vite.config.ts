04b8ea2113797a02e94e28265b4421e0471239db
Aziiimm <azimrahat108@gmail.com>
2024-11-04 00:19:25 -0500

    finalizing vercel deployment settings

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,10 +2,9 @@ import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/frontend/", // Make sure this is appropriate for your deployment
+  base: "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
